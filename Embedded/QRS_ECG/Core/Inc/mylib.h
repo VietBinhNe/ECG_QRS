a3a1d75d9394ea27b7b1e22c6abab8df276e01cc
Viet Binh <nguyenducvietbinh@gmail.com>
2025-04-10 10:32:17 +0700

    style : add template

--- Embedded/QRS_ECG/Core/Inc/mylib.h
+++ Embedded/QRS_ECG/Core/Inc/mylib.h
@@ -1,21 +1,36 @@
-/*
- * mylib.h
- *
- *  Created on: Mar 23, 2025
- *      Author: nguye
+/**
+ * @file       mylib.h
+ * @copyright  Copyright (C) 2025 HCMUS. All rights reserved.
+ * @license    This project is released under the VB's License.
+ * @version    1.0.0
+ * @date       2025-03-23
+ * @author     Binh Nguyen
+ *             
+ * @brief      Library for managing global variables and handles for STM32 ADC and UART operations.
+ *             
+ * @note       This file contains declarations of global variables used across the project.
+ * @example    main.c
+ *             Main application using ADC and UART variables.
  */
 
+/* Define to prevent recursive inclusion ------------------------------ */
 #ifndef INC_MYLIB_H_
 #define INC_MYLIB_H_
 
+/* Includes ----------------------------------------------------------- */
 #include "main.h"
 #include <string.h>
 #include <stdlib.h>
 
-extern uint8_t stream_index, array_index, send_flag;
-extern uint32_t ADC_value;
-extern UART_HandleTypeDef huart2;
-extern uint16_t ADC_Values[4][64]; // 64 sample and 4 line ?
-extern ADC_HandleTypeDef hadc1;
+/* Public variables --------------------------------------------------- */
+extern uint8_t stream_index;        /**< Index of the current stream in ADC_Values array */
+extern uint8_t array_index;         /**< Index of the current sample in the stream */
+extern uint8_t send_flag;           /**< Flag to indicate when to send data over UART */
+extern uint32_t ADC_value;          /**< Raw ADC value read from the sensor */
+extern UART_HandleTypeDef huart2;   /**< UART handle for communication */
+extern uint16_t ADC_Values[4][64];  /**< Array to store 4 streams of 64 ADC samples each */
+extern ADC_HandleTypeDef hadc1;     /**< ADC handle for reading sensor data */
 
 #endif /* INC_MYLIB_H_ */
+
+/* End of file -------------------------------------------------------- */
--- Embedded/QRS_ECG/Core/Src/mylib.c
+++ Embedded/QRS_ECG/Core/Src/mylib.c
@@ -1,13 +1,24 @@
-/*
- * mylib.c
- *
- *  Created on: Mar 23, 2025
- *      Author: nguye
+/**
+ * @file       mylib.c
+ * @copyright  Copyright (C) 2025 HCMUS. All rights reserved.
+ * @license    This project is released under the VB's License.
+ * @version    1.0.0
+ * @date       2025-03-23
+ * @author     Binh Nguyen
+ *             
+ * @brief      Implementation of global variables for STM32 ADC and UART operations.
+ *             
+ * @note       This file defines global variables declared in mylib.h.
+ * @example    main.c
+ *             Main application using ADC and UART variables.
  */
 
-
+/* Includes ----------------------------------------------------------- */
 #include "mylib.h"
 
-uint8_t stream_index = 0, array_index = 0, send_flag = 0;
-uint32_t ADC_value;
-uint16_t ADC_Values[4][64];
+/* Public variables --------------------------------------------------- */
+uint8_t stream_index = 0;       /**< Index of the current stream in ADC_Values array */
+uint8_t array_index = 0;        /**< Index of the current sample in the stream */
+uint8_t send_flag = 0;          /**< Flag to indicate when to send data over UART */
+uint32_t ADC_value;             /**< Raw ADC value read from the sensor */
+uint16_t ADC_Values[4][64];     /**< Array to store 4 streams of 64 ADC samples each */
