93eaff4433f5b5366813c99e80d47165678fc645
Viet Binh <nguyenducvietbinh@gmail.com>
2025-05-08 21:36:30 +0700

    improve : Optimize division using bit shifting

--- Embedded/QRS_ECG/Core/Src/filter.c
+++ Embedded/QRS_ECG/Core/Src/filter.c
@@ -2,15 +2,15 @@
  * @file       filter.c
  * @copyright  Copyright (C) 2025 HCMUS. All rights reserved.
  * @license    This project is released under the VB's License.
- * @version    1.0.0
+ * @version    1.0.2
  * @date       2025-04-10
  * @author     Binh Nguyen
  *
  * @brief      Implementation of signal filtering functions for STM32.
  *
- * @note       This file implements a Moving Average Filter and Bandpass Filter for smoothing and filtering ADC signals.
+ * @note       This file implements a Bandpass Filter for filtering ADC signals.
  * @example    main.c
- *             Main application using the filter to smooth ADC data.
+ *             Main application using the filter to filter ADC data.
  */
 
 /* Includes ----------------------------------------------------------- */
@@ -35,36 +35,6 @@
 /* None */
 
 /* Function definitions ----------------------------------------------- */
-void MovingAverageFilter_Init(MovingAverageFilter* filter)
-{
-    for (uint8_t i = 0; i < MOVING_AVERAGE_WINDOW_SIZE; i++) {
-        filter->buffer[i] = 0;
-    }
-    filter->index = 0;
-    filter->is_full = 0;
-}
-
-uint16_t MovingAverageFilter_Apply(MovingAverageFilter* filter, uint16_t new_sample)
-{
-    filter->buffer[filter->index] = new_sample;
-    filter->index++;
-
-    if (filter->index >= MOVING_AVERAGE_WINDOW_SIZE) {
-        filter->index = 0;
-        filter->is_full = 1;
-    }
-
-    if (!filter->is_full) {
-        return new_sample;
-    }
-
-    uint32_t sum = 0;
-    for (uint8_t i = 0; i < MOVING_AVERAGE_WINDOW_SIZE; i++) {
-        sum += filter->buffer[i];
-    }
-    return (uint16_t)(sum / MOVING_AVERAGE_WINDOW_SIZE);
-}
-
 void BandpassFilter_Init(BandpassFilter* filter)
 {
     for (uint8_t i = 0; i < BANDPASS_LOWPASS_WINDOW_SIZE; i++) {
@@ -89,7 +59,7 @@ int32_t BandpassFilter_Apply(BandpassFilter* filter, int32_t new_sample)
     for (uint8_t i = 0; i < BANDPASS_LOWPASS_WINDOW_SIZE; i++) {
         lowpass += filter->lowpass_buffer[i];
     }
-    lowpass /= BANDPASS_LOWPASS_WINDOW_SIZE;
+    lowpass = (lowpass * 205) >> 10;
 
     // High-pass filter (cutoff ~0.5 Hz at 200 Hz)
     for (uint16_t i = BANDPASS_HIGHPASS_WINDOW_SIZE - 1; i > 0; i--) {
@@ -97,15 +67,19 @@ int32_t BandpassFilter_Apply(BandpassFilter* filter, int32_t new_sample)
     }
     filter->highpass_buffer[0] = lowpass;
 
-    // Calculate the average of the previous 128 samples
     int32_t lowpass_average = 0;
     for (uint16_t i = 0; i < BANDPASS_HIGHPASS_WINDOW_SIZE; i++) {
         lowpass_average += filter->highpass_buffer[i];
     }
-    lowpass_average /= BANDPASS_HIGHPASS_WINDOW_SIZE;
+
+    lowpass_average >>= 7;
 
     // High-pass filter: y[n] = x[n] - (1/128) * sum(x[n-k])
     int32_t highpass = lowpass - lowpass_average;
+
+    // Overflow control
+    if (highpass > 32767) highpass = 32767;
+    if (highpass < -32768) highpass = -32768;
 
     return highpass;
 }
